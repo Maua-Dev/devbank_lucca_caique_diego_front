58c405342bbf0e8851091d0f02b275cf33f26868
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-04-18 15:41:15 -0300

    feat: implementa o background da pagina de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import { useState } from 'react'
 import './App.css'
+import Login from './pages/Login/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <h1>🚀 Vite React Template 🚀</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <Login>
+                
+            </Login>
+        </>
+    )
 }
 
 export default App
