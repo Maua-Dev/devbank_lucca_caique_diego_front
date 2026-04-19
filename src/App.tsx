71dc7107fff1db48bcb728cd6be61383161c66fd
Caique Frassao <caiqueoliveira341@gmail.com>
2026-04-19 15:34:36 -0300

    fix: remove conteudo do App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import './App.css'
-import Cedula from './components/Cedula/Cedula'
+
 
 function App() {
 
     return (
         <>
-            <Cedula valor={2}></Cedula>
-            <Cedula valor={5}></Cedula>
-            <Cedula valor={10}></Cedula>
-            <Cedula valor={20}></Cedula>
-            <Cedula valor={50}></Cedula>    
+             
         </>
     )
 }
