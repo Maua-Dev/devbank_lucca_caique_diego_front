cb8257d689d5d29b6f74b09e768753ae4793138b
Caique Frassao <caiqueoliveira341@gmail.com>
2026-04-19 15:08:30 -0300

    feat: implementa o css no card da cedula

--- src/components/Cedula/Cedula.tsx
+++ src/components/Cedula/Cedula.tsx
@@ -1,26 +1,24 @@
 import { useState } from "react";
 import "./Cedula.css";
+
 interface ICedula {
     valor: number;
 }
 
 export default function Cedula({ valor }: ICedula) {
     const [count, setCount] = useState(0);
+    
     return (
         <div className="container">
             <div className="cardCedula">
-                <p>R${valor}</p>
-                <div className="containerMenor">
-                    <p> Quantidade </p>
-                    <div>
-                        <button
-                            onClick={() => setCount(count <= 0 ? 0 : count - 1)}
-                        >
-                            -
-                        </button>
-                        <p>{count}</p>
-                        <button onClick={() => setCount(count + 1)}>+</button>
-                    </div>
+                <p>R$ {valor}</p>
+            </div>
+            <div className="containerMenor">
+                <p>Quantidade</p>
+                <div>
+                    <button onClick={() => setCount(count <= 0 ? 0 : count - 1)}>-</button>
+                    <span>{count}</span>
+                    <button onClick={() => setCount(count + 1)}>+</button>
                 </div>
             </div>
         </div>
