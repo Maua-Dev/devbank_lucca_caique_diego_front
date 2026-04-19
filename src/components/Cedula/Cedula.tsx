00cdb9b09c0f3f1233f4ed03b6fd5b2d03b74ba0
Caique de Oliveira Frassão <caiqueoliveira341@gmail.com>
2026-04-19 15:36:38 -0300

    Feat/implementa componente cedula (#4)
    * feat: implementa o componente de cedula e contador sem css
    
    * feat: implementa o css no card da cedula
    
    * style: muda o css do componente de cedula
    
    * style: implementa transicao e retira padding do container
    
    * fix: remove conteudo do App.tsx

---
+++ src/components/Cedula/Cedula.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "./Cedula.css";
+
+interface ICedula {
+    valor: number;
+}
+
+export default function Cedula({ valor }: ICedula) {
+    const [count, setCount] = useState(0);
+    
+    return (
+        <div className="container">
+            <div className="cardCedula">
+                <span className="spanrs">R$</span>
+                <span>{valor}</span>
+            </div>
+            <div className="containerMenor">
+                <p>Quantidade</p>
+                <div>
+                    <button onClick={() => setCount(count <= 0 ? 0 : count - 1)}>-</button>
+                    <span>{count}</span>
+                    <button onClick={() => setCount(count + 1)}>+</button>
+                </div>
+            </div>
+        </div>
+    );
+}
