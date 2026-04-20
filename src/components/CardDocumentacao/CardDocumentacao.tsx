6d1e15e34e5d882d6c6f1b13bd13af4263ba7444
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-04-19 22:21:57 -0300

    style: adiciona css na funcionalidade de clique do card

--- src/components/CardDocumentacao/CardDocumentacao.tsx
+++ src/components/CardDocumentacao/CardDocumentacao.tsx
@@ -8,30 +8,49 @@ interface ICardDocumentacao {
     request?: string;
 }
 
+function handleCardActivity() {
+    document.getElementsByClassName("card-documentacao")[0].classList.toggle("ligado");
+
+    document.getElementsByClassName("container-documentacao")[0].classList.toggle("hidden");
+    document.getElementsByClassName("desc")[0].classList.toggle("hidden");
+    document.getElementsByClassName("request")[0]?.classList.toggle("hidden");
+    document.getElementsByClassName("response")[0].classList.toggle("hidden");
+}
+
+
 function handleCardType(tipo: string) {
     if (tipo == "get") {
-        document.getElementsByClassName("request")[0].classList.add("hidden");
+        const request = document.getElementById("request")
+        if (request?.parentNode) {
+            request.parentNode.removeChild(request)
+        }
     }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function CardDocumentacao({ rota, tipo, desc, response, request }: ICardDocumentacao) {
+
     handleCardType(tipo)
     return (
         <div>
-            <button className="card-documentacao">
+            <button className="card-documentacao" onClick={() => {
+                handleCardActivity()
+                handleCardType(tipo)
+            }}>
                 {tipo} {rota}
             </button>
-            <div className={tipo}>
-                <pre className="desc">
+            <div className="container-documentacao hidden">
+                <pre className="desc hidden">
                     {desc}
                 </pre>
-                <pre className="request">
+                <pre id="request" className="request hidden">
+                    Request: <br /><br />
                     <code>
                         {request}
                     </code>
                 </pre>
-                <pre className="response">
+                <pre className="response hidden">
+                    Response: <br /><br />
                     <code>
                         {response}
                     </code>
