3bfc6eee4c3ad000ede3cb0678ddb2258a50f71f
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-04-19 23:09:07 -0300

    feat: implementa versao final do componente de CardDocumentacao

--- src/components/CardDocumentacao/CardDocumentacao.tsx
+++ src/components/CardDocumentacao/CardDocumentacao.tsx
@@ -1,6 +1,10 @@
 import "./CardDocumentacao.css";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import Icon from '@mdi/react';
+import { mdiClipboardOutline } from '@mdi/js';
 
 interface ICardDocumentacao {
+    id: string;
     tipo: string;
     rota: string;
     desc: string;
@@ -8,55 +12,63 @@ interface ICardDocumentacao {
     request?: string;
 }
 
-function handleCardActivity() {
-    document.getElementsByClassName("card-documentacao")[0].classList.toggle("ligado");
+function handleCardActivity(id: string) {
+    const card = document.getElementById(id)
+    card?.getElementsByClassName("card-documentacao")[0].classList.toggle("ligado");
 
-    document.getElementsByClassName("container-documentacao")[0].classList.toggle("hidden");
-    document.getElementsByClassName("desc")[0].classList.toggle("hidden");
-    document.getElementsByClassName("request")[0]?.classList.toggle("hidden");
-    document.getElementsByClassName("response")[0].classList.toggle("hidden");
+    card?.getElementsByClassName("container-documentacao")[0].classList.toggle("hidden");
+    card?.getElementsByClassName("desc")[0].classList.toggle("hidden");
+    card?.getElementsByClassName("request")[0]?.classList.toggle("hidden");
+    card?.getElementsByClassName("response")[0].classList.toggle("hidden");
 }
 
 
-function handleCardType(tipo: string) {
-    if (tipo == "get") {
-        const request = document.getElementById("request")
-        if (request?.parentNode) {
-            request.parentNode.removeChild(request)
-        }
+function handleCardType(tipo: string, id: string) {
+    const card = document.getElementById(id)
+    if (tipo === "get") {
+        card?.getElementsByClassName("request")[0]?.classList.toggle("hidden");
     }
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default function CardDocumentacao({ rota, tipo, desc, response, request }: ICardDocumentacao) {
+function handleCopy(classe: string, id: string) {
+    const card = document.getElementById(id)
+    card?.getElementsByClassName(classe)[0].classList.toggle("hidden");
+}
 
-    handleCardType(tipo)
+export default function CardDocumentacao({ rota, tipo, desc, response, request, id }: ICardDocumentacao) {
     return (
-        <div>
+        <div id={id}>
             <button className="card-documentacao" onClick={() => {
-                handleCardActivity()
-                handleCardType(tipo)
+                handleCardActivity(id)
+                handleCardType(tipo, id)
             }}>
                 {tipo} {rota}
             </button>
+
             <div className="container-documentacao hidden">
                 <pre className="desc hidden">
                     {desc}
                 </pre>
-                <pre id="request" className="request hidden">
-                    Request: <br /><br />
-                    <code>
-                        {request}
-                    </code>
-                </pre>
-                <pre className="response hidden">
-                    Response: <br /><br />
-                    <code>
-                        {response}
-                    </code>
-                </pre>
+                <CopyToClipboard text={request ? request : "Nada para copiar"} >
+                    <pre id="request" className="request hidden" onMouseEnter={() => handleCopy("icone-request", id)} onMouseLeave={() => handleCopy("icone-request", id)}>
+                        <Icon className="icone-request hidden" path={mdiClipboardOutline} size={1} />
+                        Request: <br /><br />
+                        <code>
+                            {request}
+                        </code>
+                    </pre>
+                </CopyToClipboard>
+                <CopyToClipboard text={response}>
+                    <pre className="response hidden" onMouseEnter={() => handleCopy("icone-response", id)} onMouseLeave={() => handleCopy("icone-response", id)}>
+                        <Icon className="icone-response hidden" path={mdiClipboardOutline} size={1} />
+                        Response: <br /><br />
+                        <code>
+                            {response}
+                        </code>
+                    </pre>
+                </CopyToClipboard>
             </div>
-        </div>
+        </div >
 
     )
 }
