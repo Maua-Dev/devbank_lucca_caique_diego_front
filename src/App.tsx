5e07486ea7c6d91ed2023bdfcee4a239f291a1fc
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-04-19 21:38:57 -0300

    implementa o html da funcionalidade de clique

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,32 @@ function App() {
 
     return (
         <>
-            <CardDocumentacao tipo='post' rota='/deposit'></CardDocumentacao>
+            <CardDocumentacao
+            tipo='post'
+            rota='/deposit'
+            desc='Trata-se de um POST, são passados a quantidade de células, se não possuir 
+a CHAVE daquele valor, a quantidade é 0. O valor deve ser depositado 
+na conta do usuário. Caso o valor depositado seja o dobro da quantidade em conta
+deve retornar o status code "403" (Forbidden) e uma string "Depósito suspeito".
+Em casos convencionais retorna os seguintes valores: 
+
+  current_balance [float] - valor atual na conta (após o depósito), 
+  timestamp[float] - instante da operação em milissegundos' 
+            response='{
+        "current_balance": 1000.0,
+        "timestamp": 1690482853890 
+}'
+            request='{
+        "2": 1,
+        "5": 2,
+        "10": 3,
+        "20": 4,
+        "50": 5 ,
+        "100": 6,
+        "200": 7
+}'>
+
+            </CardDocumentacao>
         </>
     )
 }
--- src/components/CardDocumentacao/CardDocumentacao.tsx
+++ src/components/CardDocumentacao/CardDocumentacao.tsx
@@ -3,12 +3,41 @@ import "./CardDocumentacao.css";
 interface ICardDocumentacao {
     tipo: string;
     rota: string;
+    desc: string;
+    response: string;
+    request?: string;
 }
 
-export default function CardDocumentacao({rota, tipo}:ICardDocumentacao) {
+function handleCardType(tipo: string) {
+    if (tipo == "get") {
+        document.getElementsByClassName("request")[0].classList.add("hidden");
+    }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default function CardDocumentacao({ rota, tipo, desc, response, request }: ICardDocumentacao) {
+    handleCardType(tipo)
     return (
-        <button className="card-documentacao">
-            {tipo} {rota}
-        </button>
+        <div>
+            <button className="card-documentacao">
+                {tipo} {rota}
+            </button>
+            <div className={tipo}>
+                <pre className="desc">
+                    {desc}
+                </pre>
+                <pre className="request">
+                    <code>
+                        {request}
+                    </code>
+                </pre>
+                <pre className="response">
+                    <code>
+                        {response}
+                    </code>
+                </pre>
+            </div>
+        </div>
+
     )
 }
