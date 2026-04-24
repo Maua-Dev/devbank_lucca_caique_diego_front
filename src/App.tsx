eb8aa0c7727bedab4ea6d31f4057921bbd445b3d
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-04-24 14:21:46 -0300

    fix: remove coisas inuteis do app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import './App.css'
-import Transacao from './components/Transacao/Transacao'
-
 function App() {
 
     return (
         <>
-            <Transacao data={new Date()} saldo={10} texto='Saque' valor={100} key={1} ></Transacao>
+            
         </>
     )
 }
