564457ef2fa6384a829f64243029911d4a930d9f
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-04-24 14:08:09 -0300

    feat: adiciona a funcionalidade de inserir data no componente de transacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Transacao from './components/Transacao/Transacao'
 
 function App() {
 
     return (
         <>
-
+            <Transacao data={new Date()} saldo={10} texto='Saque' valor={100} key={1} ></Transacao>
         </>
     )
 }
--- src/components/Transacao/Transacao.tsx
+++ src/components/Transacao/Transacao.tsx
@@ -3,7 +3,27 @@ import "./Transacao.css";
 interface ITransacao {
     texto: string;
     valor: number;
+    data: Date,
     saldo: number;
+}
+
+function formatarNum(numero: number): string {
+    return String(numero).padStart(2, "0");
+}
+
+const meses = {
+    "0": "Jan",
+    "1": "Fev",
+    "2": "Mar",
+    "3": "Abr",
+    "4": "Mai",
+    "5": "Jun",
+    "6": "Jul",
+    "7": "Ago",
+    "8": "Set",
+    "9": "Out",
+    "10": "Nov",
+    "11": "Dez",
 }
 
 function handleCardColor(texto: string): string {
@@ -14,9 +34,12 @@ function handleCardColor(texto: string): string {
     return cor;
 }
 
-export default function Transacao({ texto, valor, saldo }: ITransacao) {
+export default function Transacao({ texto, valor, data, saldo }: ITransacao) {
 
     const cor = handleCardColor(texto)
+
+    console.log(data)
+    const mes = meses[data.getMonth()]
 
     return (
         <div className="card">
@@ -30,7 +53,7 @@ export default function Transacao({ texto, valor, saldo }: ITransacao) {
                 </div>
                 <div className="Data">
                     <p>DATA:</p>
-                    <span>Apr 22 2026 16:00:00 </span>
+                    <span>{mes} {data.getDate()} {data.getFullYear()} {formatarNum(data.getHours())}:{formatarNum(data.getMinutes())}:{formatarNum(data.getSeconds())}</span>
                 </div>
                 <div className="Saldo">
                     <p>SALDO:</p>
