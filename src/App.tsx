49fee89c30c8b7779d33b868ef6969837fb2f5ee
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-04-28 14:15:33 -0300

    feat: implementa emoji de lapis para editar a api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { useState } from 'react'
 import './App.css'
+import InputAPI from './components/InputAPI/InputAPI'
+import { FaPencilAlt } from "react-icons/fa";
 
 function App() {
 
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [text, setText] = useState("")
+
     return (
         <>
-            
+            <InputAPI onChange={(e) => setText(e.target.value)} value='https://y5klzz3x33bqv3kytn4swkcwji0vlfhw.lambda-url.us-east-1.on.aws'><button><FaPencilAlt /></button></InputAPI>
+            <InputAPI onChange={(e) => setText(e.target.value)} value=''></InputAPI>
         </>
     )
 }
--- src/components/Documentacao/Documentacao.tsx
+++ src/components/Documentacao/Documentacao.tsx
@@ -1,20 +1,27 @@
 import "./Documentacao.css"
+import { useEffect } from "react"
 
 interface IDocumentacao {
     fixo: boolean;
 }
 
 export default function Documentacao({ fixo }: IDocumentacao) {
-    if (fixo) {
+    useEffect(() => {
         const doc = document.getElementById("documentacao")
-        if (doc) {
+        if (doc && fixo) {value=''
             doc.style.position = "absolute"
             doc.style.right = "32px"
             doc.style.top = "32px"
         }
+    }, [fixo])
 
-    }
     return (
-        <button className="documentacao" id="documentacao">?</button>
+        <button
+            className="documentacao"
+            id="documentacao"
+            style={fixo ? { position: "absolute", right: "32px", top: "32px" } : {}}
+        >
+            ?
+        </button>
     )
 }
