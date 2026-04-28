d720b675ec5bb811756c22cb0302c9fac3e446ad
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-04-27 21:16:03 -0300

    fix: remove o conteudo desnecessario do app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import './App.css'
-import CardSaldo from './components/CardSaldo/CardSaldo'
 
 
 function App() {
 
     return (
         <>
-            <CardSaldo texto = 'oi' boxSide='right'></CardSaldo>
+
         </>
     )
 }
