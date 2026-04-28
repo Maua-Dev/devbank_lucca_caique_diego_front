d563b072428585f1f76913430adcc1396bd6f5fe
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-04-28 10:20:18 -0300

    fix: corrige erro de deployment na aws

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Transacao from "./components/Transacao/Transacao"
+
 function App() {
 
     return (
         <>
-            
+            <Transacao saldo={10} data={new Date()} texto="Saque" valor={100}></Transacao>
         </>
     )
 }
--- src/components/Transacao/Transacao.tsx
+++ src/components/Transacao/Transacao.tsx
@@ -11,21 +11,6 @@ function formatarNum(numero: number): string {
     return String(numero).padStart(2, "0");
 }
 
-const meses = {
-    "0": "Jan",
-    "1": "Fev",
-    "2": "Mar",
-    "3": "Abr",
-    "4": "Mai",
-    "5": "Jun",
-    "6": "Jul",
-    "7": "Ago",
-    "8": "Set",
-    "9": "Out",
-    "10": "Nov",
-    "11": "Dez",
-}
-
 function handleCardColor(texto: string): string {
     let cor: string = "red";
     if (texto == "Deposito") {
@@ -37,9 +22,7 @@ function handleCardColor(texto: string): string {
 export default function Transacao({ texto, valor, data, saldo }: ITransacao) {
 
     const cor = handleCardColor(texto)
-
-    console.log(data)
-    const mes = meses[data.getMonth()]
+    const mes = data.toLocaleDateString('pt-BR', { month: 'long' }).slice(0, 3);
 
     return (
         <div className="card">
