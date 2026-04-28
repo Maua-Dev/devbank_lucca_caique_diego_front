f5712674b186db28cfd4db3b9067c2d17abb933a
Lucca de Barros Rodrigues <luccamauarodrigues@gmail.com>
2026-04-28 10:24:15 -0300

    fix: ajusta a definição de tipo no componente de InputAPI (#8)

--- src/components/InputAPI/InputAPI.tsx
+++ src/components/InputAPI/InputAPI.tsx
@@ -1,21 +1,19 @@
 import "./InputAPI.css"
-import type { ChangeEventHandler } from "react"
-
 interface IInputAPI {
     value: string;
-    onChange: ChangeEventHandler<HTMLInputElement>;
+    onChange: (e) => void;
 }
 
 export default function InputAPI({ value, onChange }: IInputAPI) {
     return (
         <input
-        type="text"
-        name="inputAPI"
-        id="inputAPI"
-        placeholder="Coloque aqui o endpoint da sua API"
-        className="inputAPI"
-        value={value}
-        onChange={onChange}
+            type="text"
+            name="inputAPI"
+            id="inputAPI"
+            placeholder="Coloque aqui o endpoint da sua API"
+            className="inputAPI"
+            value={value}
+            onChange={onChange}
         />
     )
 }
