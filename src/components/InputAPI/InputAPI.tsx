ac387223257b3614e2744506fc5a867eb6ab5acb
Lucca de Barros Rodrigues <luccamauarodrigues@gmail.com>
2026-04-30 18:09:01 -0300

    feat: implementa localstorage itens na tela de login, no componentes de topbar e cardsaldo (#25)

--- src/components/InputAPI/InputAPI.tsx
+++ src/components/InputAPI/InputAPI.tsx
@@ -4,7 +4,7 @@ import { ChangeEvent, PropsWithChildren, ReactNode } from "react"
 
 interface IInputAPI {
     value?: string;
-    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
     children?: PropsWithChildren<ReactNode>;
     readOnly?: boolean;
 }
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -5,12 +5,20 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import Botao from "../../components/Botao/Botao"
 import Documentacao from "../../components/Documentacao/Documentacao"
+import { Users } from "../../mock/User"
 
 export default function Login() {
 
     const [value, setValue] = useState("")
     const [fixo] = useState(true)
     const navigate = useNavigate()
+
+    function handleLocalStorage(): void {
+        localStorage.setItem("saldo", "1000");
+        localStorage.setItem("nome", Users.user1.nome);
+        localStorage.setItem("agencia", Users.user1.agencia);
+        localStorage.setItem("conta", Users.user1.conta);
+    }
 
     function handleLogin(url: string) {
         if (url != "https://y5klzz3x33bqv3kytn4swkcwji0vlfhw.lambda-url.us-east-1.on.aws") {
@@ -31,7 +39,7 @@ export default function Login() {
 
                 <InputAPI onChange={(e) => setValue(e.target.value)} value={value}></InputAPI>
                 <p className="p-aviso" id="p-aviso"></p>
-                <Botao texto="Entrar" onClick={() => handleLogin(value)}></Botao>
+                <Botao texto="Entrar" onClick={() => { handleLogin(value); handleLocalStorage() }}></Botao>
             </div>
         </div>
     )
