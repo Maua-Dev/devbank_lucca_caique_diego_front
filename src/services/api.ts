1c08ff0e8e89b4506db883a34f0a4dbf26a01acb
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-04-30 19:15:10 -0300

    feat: implementa funcao de login usando api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-export const BASE_URL: string = sessionStorage.getItem("URL_API") ?? "";
+export const BASE_URL: string = sessionStorage.getItem("URL_API") + "/";
 
 export async function get(endpoint: string = "") {
     const response = await fetch(`${BASE_URL}${endpoint}`);
